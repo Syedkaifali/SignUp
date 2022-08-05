271427bd85ca1ec5b13d610e2f3c218ae845a321
Syedkaifali <76244144+Syedkaifali@users.noreply.github.com>
2022-08-05 05:32:28 +0530

    hi

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.loginn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openlogin_page();
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.signupbtn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opensignin_page();
+            }
+        });
+
     }
+    public void openlogin_page(){
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
+    }
+    public void opensignin_page(){
+        Intent intent = new Intent(this, Signuppage.class);
+        startActivity(intent);
+    }
+
 }
