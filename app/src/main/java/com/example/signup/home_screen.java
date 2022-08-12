074b5f2db986f18a742e4feef7f5c5ecd62911d6
siddharth0402 <siddharthsonkusre@gmail.com>
2022-08-12 13:20:21 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/google-services.json
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/signup/LoginPage.java
    #	app/src/main/java/com/example/signup/MainActivity.java
    #	app/src/main/res/layout/activity_login_page.xml
    #	app/src/main/res/layout/activity_signuppage.xml

--- app/src/main/java/com/example/signup/home_screen.java
+++ app/src/main/java/com/example/signup/home_screen.java
@@ -2,18 +2,27 @@ package com.example.signup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class home_screen extends AppCompatActivity {
+public class home_screen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button scan_button;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     Toolbar toolbar;
 
 private FirebaseAuth mFirebaseAuth;
@@ -22,8 +31,18 @@ private FirebaseAuth mFirebaseAuth;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         Button button = findViewById(R.id.logout);
@@ -43,6 +62,16 @@ private FirebaseAuth mFirebaseAuth;
 
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if ( drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
 
 
     private void openHistory() {
@@ -66,4 +95,23 @@ private FirebaseAuth mFirebaseAuth;
         startActivity(intent);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_upload:
+                Intent intent = new Intent(home_screen.this, upd.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_logout:
+                mFirebaseAuth.signOut();
+                Intent intent2 = new Intent(this, MainActivity.class);
+                startActivity(intent2);
+        }
+        return true;
+    }
 }
