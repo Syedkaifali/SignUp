7bfda9abf822ceea413bc6b6792f46843dc1d041
siddharth0402 <siddharthsonkusre@gmail.com>
2022-08-25 23:52:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/google-services.json
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/signup/LoginPage.java
    #	app/src/main/java/com/example/signup/MainActivity.java
    #	app/src/main/res/layout/activity_login_page.xml
    #	app/src/main/res/layout/activity_signuppage.xml

--- app/src/main/java/com/example/signup/home_screen.java
+++ app/src/main/java/com/example/signup/home_screen.java
@@ -1,11 +1,16 @@
 package com.example.signup;
 
+import static com.example.signup.G.context;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -18,12 +23,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class home_screen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class home_screen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener   {
 
-    private Button scan_button;
+    private Button scan_button, hospitals_nearby;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
 
 private FirebaseAuth mFirebaseAuth;
     @Override
@@ -59,6 +65,22 @@ private FirebaseAuth mFirebaseAuth;
         buttonView.setOnClickListener(view -> openHistory());
         ImageButton buttonB = findViewById(R.id.beds);
         buttonB.setOnClickListener((view ->openHospital()));
+        hospitals_nearby = (Button) findViewById(R.id.BUTS);
+        hospitals_nearby.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = "geo:0, 0?q= Hospitals Nearby";
+                Toast.makeText(context,location,Toast.LENGTH_SHORT).show();
+                Log.d("MAPS",location);
+                Uri uri = Uri.parse(location);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
+
+//        hospitals_nearby.setOnClickListener((View.OnClickListener) this);
+
         scan_button= (Button) findViewById(R.id.scan_button);
         scan_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +88,6 @@ private FirebaseAuth mFirebaseAuth;
                 openscan_page();
             }
         });
-
     }
 
     private void openHospital() {
