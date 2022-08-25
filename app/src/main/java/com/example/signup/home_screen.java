e0569e816f8333070b7a0cadd9602d64e254b936
siddharth0402 <siddharthsonkusre@gmail.com>
2022-08-26 03:36:28 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/google-services.json
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/signup/LoginPage.java
    #	app/src/main/java/com/example/signup/MainActivity.java
    #	app/src/main/res/layout/activity_login_page.xml
    #	app/src/main/res/layout/activity_signuppage.xml

--- app/src/main/java/com/example/signup/home_screen.java
+++ app/src/main/java/com/example/signup/home_screen.java
@@ -2,7 +2,9 @@ package com.example.signup;
 
 import static com.example.signup.G.context;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +14,13 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -89,6 +93,16 @@ private FirebaseAuth mFirebaseAuth;
             }
         });
     }
+
+    public void BtnSetEmergency_onClick(View V){
+        String number ="+919926885790";
+        Intent intentcall = new Intent( Intent.ACTION_CALL);
+        intentcall.setData(Uri.parse("tel:"+ number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED);
+        startActivity(intentcall);
+    }
+
+
 
     private void openHospital() {
         Intent intent = new Intent(this, Hhomescreen.class);
